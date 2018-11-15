bece2ddf2b99e137de69ff8f8fc874bd22505326
DESKTOP-17COGJ5\admin <luis.gong@cardinfolink.com>
2018-11-15 14:13:57 +0800

    do something in master

--- app/src/main/java/com/cardinfolink/application/MainActivity.java
+++ app/src/main/java/com/cardinfolink/application/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //i'm fine ，and you ?
+        //i have do some thing in master
     }
 }
