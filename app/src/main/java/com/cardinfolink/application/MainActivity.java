336579f14b45d23d6511081b6611f74508d5a6b6
DESKTOP-17COGJ5\admin <luis.gong@cardinfolink.com>
2018-11-15 14:11:37 +0800

    up sdk

--- app/src/main/java/com/cardinfolink/application/MainActivity.java
+++ app/src/main/java/com/cardinfolink/application/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //i'm fine ，and you ?
+
+        //
     }
 }
